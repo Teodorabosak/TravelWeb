55e46cd5a6fa340a4556e324f6a063a3140d84bb
Teodorabosak <teodora.bosak@gmail.com>
2024-03-04 17:02:57 +0100

    4.3.2024. 17h

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container, nasa aplikacija koristi kontrolere
+// Dependency injection ide ovde
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -23,5 +24,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+// kakva je ruta, ako nije nista definisano onda je ovo ruta, ? znaci da moze  biti definisana i ne mora
 
 app.Run();
+//pokrece aplikaciju
