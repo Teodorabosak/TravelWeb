b9031ad1524dc0e473b903cba67ac38cfef32f18
Teodorabosak <teodora.bosak@gmail.com>
2024-04-02 19:32:39 +0200

    Radi sve za identity namesteni paketi na 8.0.3

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -16,6 +16,8 @@ namespace TravelWeb.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Letovanje", DisplayOrder = 1, Description = "" },
                 new Category { Id = 2, Name = "Gradovi Europe", DisplayOrder = 2, Description = "Uzmite pauzu od svakodnevnice i zaronite u čarobni svet evropskih gradova uz našu turističku agenciju. Prepustite se avanturi i istražite fascinantnu istoriju, bogatu kulturu i neponovljivu atmosferu nekih od najpoznatijih destinacija u Evropi. Sa našim pažljivo odabranim putovanjima, očekuju vas nezaboravna iskustva i trenuci koji će vas inspirisati i osvežiti. Doživite čaroliju Pariza, osetite duh Rima, prošetajte kroz šarmantne ulice Amsterdama i još mnogo toga. Vaše putovanje počinje ovde!" },
